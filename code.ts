ed7e3c14eb10a048a776283aa89a1234746a96e3
unknown <moein@pro.ai>
2023-04-15 16:47:39 +0430

    messaging protocols implemented

--- code.ts
+++ code.ts
@@ -17,17 +17,9 @@ if (figma.editorType === 'figma') {
   figma.ui.onmessage = msg => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < msg.count; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+    if(msg?.type === "data_recieved"){
+      console.log("msg received from plugin");
+      
     }
 
     // Make sure to close the plugin when you're done. Otherwise the plugin will
@@ -80,7 +72,10 @@ if (figma.editorType === 'figma') {
       figma.currentPage.selection = nodes;
       figma.viewport.scrollAndZoomIntoView(nodes);
     }
-
+    if(msg?.type === "data_recieved"){
+      console.log("msg received from plugin");
+      
+    }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     figma.closePlugin();
